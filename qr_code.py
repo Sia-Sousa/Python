dc51a880c5bc3814e03793435f6835caca8fe017
Sofia Sousa <137227771+Sia-Sousa@users.noreply.github.com>
2025-11-15 16:18:07 +0000

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,6 +1,6 @@
 import qrcode
 
-website_link = 'https://www.youtube.com/watch?v=L4rOBdjcwLc&list=RDL4rOBdjcwLc&start_radio=1'
+website_link = '' # insert the desired link
 
 qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 # The version parameter is an integer from 1 to 40 that controls the size of the QR code.
@@ -10,7 +10,5 @@ qr = qrcode.QRCode(version = 1, box_size = 5, border = 5)
 qr.add_data(website_link)
 qr.make() # .make() generate the QR code
 
-img = qr.make_image(fill_color = 'black', back_color = 'white') # save this created QR code in an img pillow object
-# using qr.make_image()
-img.save('youtube_qr.png') # store and save the file. We can do this using pillow's save() command. We specify the file
-# name inside the brackets, which is youtube_qr.png
+img = qr.make_image(fill_color = 'black', back_color = 'white') # save this created QR code in an img pillow object using qr.make_image()
+img.save('youtube_qr.png') # store and save the file. We can do this using pillow's save() command. We specify the file name inside the brackets, which is youtube_qr.png
